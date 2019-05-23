1218aeebaab08af7c0e46c5ab0cf75fc17978661
Ken Chan <kjchan@udel.edu>
2019-05-23 16:09:31 -0400

    MyIterator question

---
+++ MyIterator.java
@@ -0,0 +1,13 @@
+package midterm1;
+
+import java.util.*;
+//what are the two methods one need when implementing Iterator?
+public class MyIterator implements Iterator{
+
+	
+	public static void main(String [] args) {
+		Iterator mi = new MyIterator();
+		//Print out 0->100, some code has already been filled out, finish the code using only the options
+		//found in MyIterator.jpg
+	}
+}
